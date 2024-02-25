75158e686e9e3436d824175d26e28781245862ce
modusami <128193218+modusami@users.noreply.github.com>
2024-02-25 17:13:27 -0500

    Add Header to App return

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import CardGrid from "./components/CardGrid"
-
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
+      <Header />
       <CardGrid />
     </>
   )
