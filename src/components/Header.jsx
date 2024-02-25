8597db8b42a6ac6470686860181969e64bff6d6c
modusami <128193218+modusami@users.noreply.github.com>
2024-02-25 17:30:29 -0500

    Add Classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@
 const Header = () => {
     return (
         <header className="header-div">
-            <h1>ProgrammerCentral</h1>
-            <h3>Your Go-To Hub for Top Programmer YouTube Channels!</h3>
+            <h1 id="title">ProgrammerCentral</h1>
+            <h3 id="subtitle">Your Go-To Hub for Top Programmer YouTube Channels!</h3>
         </header>
     )
 }
