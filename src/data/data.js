3582c2850ddf8b16f4fa7da7939bcf4dabf02def
modusami <128193218+modusami@users.noreply.github.com>
2024-02-25 13:45:51 -0500

    Upload Data.js

---
+++ src/data/data.js
@@ -0,0 +1,75 @@
+
+const YOUTUBER_DATA = [
+    // done
+    {
+        "name": "Dan Vega",
+        "subscribers": "47.6k",
+        "link": "https://www.youtube.com/@DanVega",
+        "img": "../assets/danvega.jpg"
+    },
+    // done
+    {
+        "name": "Web Dev Cody",
+        "subscribers": "194k",
+        "link": "https://www.youtube.com/@WebDevCody",
+        "img": "../assets/webdevcody.jpg"
+    },
+    // done
+    {
+        "name": "freecodecamp",
+        "subscribers": "9.12M",
+        "link": "https://wwwfyr.youtube.com/@freecodecamp",
+        "img": "../assets/freecodecamp.jpg"
+    },
+
+    // done
+    {
+        "name": "Frying Pan",
+        "subscribers": "335k",
+        "link": "https://www.youtube.com/@FryingPan",
+        "img": "../assets/fryingpan.jpg"
+    },
+    // done
+    {
+        "name": "Fireship",
+        "subscribers": "2.84M",
+        "link": "https://www.youtube.com/@Fireship",
+        "img": "../assets/fireshipfire.jpg"
+    },
+    // done
+    {
+        "name": "Bro Code",
+        "subscribers": "1.64M",
+        "link": "https://www.youtube.com/@BroCodez",
+        "img": "../assets/brocode.jpg"
+    },
+    // done
+    {
+        "name": "Web Dev Cody",
+        "subscribers": "194k",
+        "link": "https://www.youtube.com/@WebDevCody",
+        "img": "../assets/webdevcody.jpg"
+    },
+    // done
+    {
+        "name": "The Cherno",
+        "subscribers": "622k",
+        "link": "https://www.youtube.com/@TheCherno",
+        "img": "../assets/cherno.jpg"
+    },
+    // done
+    {
+        "name": "Tech With Time",
+        "subscribers": "1.45M",
+        "link": "https://www.youtube.com/@TechWithTim",
+        "img": "../assets/techwithtim.jpg"
+    },
+    // done
+    {
+        "name": "bigboxSWE",
+        "subscribers": "140k",
+        "link": "https://www.youtube.com/@bigboxSWE",
+        "img": "../assets/bigboxswe.jpg"
+    },
+]
+export default YOUTUBER_DATA;
