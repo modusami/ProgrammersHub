44e529e6a2c9924e694e810c82a7f8e9f1add8fe
modusami <128193218+modusami@users.noreply.github.com>
2024-02-25 18:16:05 -0500

    Add Filter For CSS Style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@
 const Card = (props) => {
     return (
         <div className="card">
+            <div className="banner"></div>
             <a className="card-link" href={props.link} target="_blank">
                 <img className="card-link-img" src={props.image} alt="none" />
             </a>
             <h1 className="card-name">{props.name}</h1>
-            <h3 className="card-sub">Subscribers: {props.subs}</h3>
+            <h3 className={"card-sub" + (props.subs.includes("M") ? " has-M" : "")}>Subscribers: {props.subs}</h3>
+
         </div>
     )
 }
