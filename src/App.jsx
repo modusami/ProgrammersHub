e073a2937d3c8ed0fd2639b40d152e1368ab9c01
modusami <128193218+modusami@users.noreply.github.com>
2024-02-25 13:58:53 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import YOUTUBER_DATA from './data/data.js';
 import './App.css'
+import CardGrid from "./components/CardGrid"
 
 
 function App() {
 
   return (
     <>
-      <h1>Example</h1>
-      {YOUTUBER_DATA.map((value, i) => {
-        return (
-          <div key={i}>
-            <h1>{value.name}</h1>
-            <h3>{value.subscribers}</h3>
-            <img src={value.img} alt="" />
-          </div>
-
-        )
-      })}
+      <CardGrid />
     </>
   )
 }
