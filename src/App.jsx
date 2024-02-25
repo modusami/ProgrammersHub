73599264cb432f406d21981880a85bf9e628839b
modusami <128193218+modusami@users.noreply.github.com>
2024-02-25 13:55:49 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import YOUTUBER_DATA from './data/data.js';
 import './App.css'
+
 
 function App() {
 
   return (
     <>
+      <h1>Example</h1>
+      {YOUTUBER_DATA.map((value, i) => {
+        return (
+          <div key={i}>
+            <h1>{value.name}</h1>
+            <h3>{value.subscribers}</h3>
+            <img src={value.img} alt="" />
+          </div>
 
+        )
+      })}
     </>
   )
 }
